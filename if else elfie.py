190f66764f4ef4d9e324b4dd1a9afa3eb1a96bb8
mayalis2002 <mayalis2002@gmail.com>
2024-10-30 13:06:24 +0200

    Add files via upload

---
+++ if else elfie.py
@@ -0,0 +1,24 @@
+# START if-else
+num: int = int(input("enter num"))
+
+if num > 0:
+    # YES
+    print(num, 'is positive')
+else:
+    # NO
+    if num == 0:
+        print('negative')
+
+# STOP
+
+# START ELIF
+num: int = int(input("enter num"))
+
+if num > 0:
+    # YES
+    print(num, 'is positive')
+elif num == 0:
+    #YES
+    print('negative')
+
+# STOP
